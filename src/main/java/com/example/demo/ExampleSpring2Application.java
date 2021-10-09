96d8193f032aec92a7cdd54af68df1360829bf1b
xbb123 <xbb123@naver.com>
2021-10-09 11:05:34 +0900

    init

---
+++ src/main/java/com/example/demo/ExampleSpring2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleSpring2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleSpring2Application.class, args);
+	}
+
+}
